21b3e1699dd07813f1daede606d6c3b742f388ad
EwkaF <obarka@interia.pl>
2019-04-15 16:16:19 +0200

    UserList component added

---
+++ src/containers/UserList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class UserList extends Component {
+  render() {
+    return (
+      <div>
+        Tu bedzie lista
+      </div>
+    )
+  }
+}
+
+export default UserList
