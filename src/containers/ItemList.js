5fc48d5b75280aff8d9a84525309c2bc16491ab9
EwkaF <obarka@interia.pl>
2019-04-25 11:13:33 +0200

    item list done

--- src/containers/ItemList.js
+++ src/containers/ItemList.js
@@ -1,27 +1,66 @@
 import React, { Component } from 'react'
-import {List, ListItem, ListItemText, ListItemSecondaryAction, IconButton,Edit, Delete} from '@material-ui/core';
+import { Grid, List, ListItem, ListItemText, ListItemSecondaryAction, IconButton, Edit, Delete, Typography } from '@material-ui/core';
 
 export class ItemList extends Component {
   render() {
     return (
       <List component="ul">
-      {this.props.items.map((element, index) => 
-        <ListItem
-        key={index}
-        button
-        // onClick={() => onSelect(id)}
-      >
-        <ListItemText primary={element.name} />
-        <ListItemSecondaryAction>
-          <IconButton>
-            {/* <Edit /> */}
-          </IconButton>
-          <IconButton >
-            {/* <Delete /> */}
-          </IconButton>
-        </ListItemSecondaryAction>
+      <ListItem>
+        <ListItemText>
+          <Grid container>
+          <Grid item xs>
+                 <Typography variant="display1">Name</Typography>
+                 </Grid>
+                 <Grid item xs>
+                 <Typography variant="display1">description</Typography>
+                 </Grid>
+                 <Grid item xs>
+                 <Typography variant="display1"> Date of experince</Typography>
+                 </Grid>
+                 <Grid item xs>
+                 <Typography variant="display1"> Category</Typography>
+                 </Grid>
+          </Grid>
+        </ListItemText>
       </ListItem>
-      )}
+    
+        {this.props.items.map((element, index) =>
+          <ListItem
+            key={index}
+            button
+          // onClick={() => onSelect(id)}
+          >
+            <ListItemText
+              primary={<Grid
+                container>
+                  <Grid item xs>
+                 <Typography variant="subtitle1"> {element.name}</Typography>
+                 </Grid>
+                 <Grid item xs>
+                 <Typography variant="subtitle1"> {element.description}</Typography>
+                 </Grid>
+                 <Grid item xs>
+                 <Typography variant="subtitle2"> {element.dateOfExp}</Typography>
+                 </Grid>
+                 <Grid item xs>
+                 <Typography variant="subtitle2"> {element.category}</Typography>
+                 </Grid>
+
+
+
+              </Grid>}
+
+            />
+            <ListItemSecondaryAction>
+              <IconButton>
+                {/* <Edit /> */}
+              </IconButton>
+              <IconButton >
+                {/* <Delete /> */}
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        )}
       </List>
     )
   }
