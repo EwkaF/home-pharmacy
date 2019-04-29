29db86136ea6be986c4f53635a999819b8757167
EwkaF <obarka@interia.pl>
2019-04-29 11:09:37 +0200

    working on POST new item

--- src/containers/AddNewMedcine.js
+++ src/containers/AddNewMedcine.js
@@ -1,49 +1,52 @@
 import React, { Component } from 'react'
-import { Dialog, DialogTitle, DialogContent, DialogActions, DialogContentText, Button} from '@material-ui/core';
+import { Dialog, DialogTitle, DialogContent, DialogActions, DialogContentText, Button } from '@material-ui/core';
 import Form from './Form'
 
 export class AddNewMedcine extends Component {
-    constructor(){
-        super();
-        this.state={
-            open: false
-        }
+  constructor() {
+    super();
+    this.state = {
+      open: false
     }
+  }
 
-    handleToggle = () => {
-        this.setState({ 
-            open: !this.state.open });
-      };
+  handleToggle = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
 
-      handleSubmit =(details) => {
-        console.log(details)
-        fetch(' http://localhost:3004/users/' + this.props.user + '/medicinesList', {
-          method: "POST",
-          body: JSON.stringify(details),
-          headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-          },
-      }).then(response => response.json())
-      .then(data => {
-              console.log("Successful" + data);
-
-          });
-      
-        this.handleToggle();
+  handleSubmit = (details) => {
+    console.log(details)
+    fetch(' http://localhost:3004/users/' + this.props.user + '/medicinesList', {
+      method: "POST",
+      body: JSON.stringify(details),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
       }
+    })
+    .then(response => console.log(response))
+      // response.json())
+      // .then(data => {
+      //   console.log("Successful" + data);
 
-      
+      // });
+
+    this.handleToggle();
+  }
+
+
   render() {
     return (
-        <div>
+      <div>
         <Button variant="outlined" color="primary" onClick={this.handleToggle}>
           Add new medcine
         </Button>
         <Dialog
           open={this.state.open}
           onClose={this.handleToggle}
-         
+
         >
           <DialogTitle id="form-dialog-title">Add new medcine
 </DialogTitle>
@@ -51,7 +54,7 @@ export class AddNewMedcine extends Component {
             <DialogContentText>
               Enter your new medcines details
             </DialogContentText>
-            <Form user={this.props.user} onSubmit={this.handleSubmit}/>
+            <Form user={this.props.user} onSubmit={this.handleSubmit} />
           </DialogContent>
           {/* <DialogActions>
             <Button onClick={this.handleClick} color="primary">
