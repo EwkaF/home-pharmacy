79bb10072c5b3f52d795109f4f5cb4250f350faa
EwkaF <obarka@interia.pl>
2019-05-08 09:47:55 +0200

    working on edit form

--- src/containers/ItemList.js
+++ src/containers/ItemList.js
@@ -1,73 +1,83 @@
 import React, { Component } from 'react'
-import { Grid, List, ListItem, ListItemText, ListItemSecondaryAction, IconButton,Typography } from '@material-ui/core';
+import { Grid, List, ListItem, ListItemText, ListItemSecondaryAction, IconButton, Typography } from '@material-ui/core';
 import { Edit, Delete } from '@material-ui/icons';
+import Form from './Form'
+import Item from './Item'
+
 
 export class ItemList extends Component {
 
-  // handleClick =() =>{
-  //   this.props.onDelete(id)
-  // }
   render() {
     return (
-      <List component="ul">
-      <ListItem>
-        <ListItemText>
-          <Grid container>
-          <Grid item xs>
-                 <Typography variant="display1">Name</Typography>
-                 </Grid>
-                 <Grid item xs>
-                 <Typography variant="display1">description</Typography>
-                 </Grid>
-                 <Grid item xs>
-                 <Typography variant="display1"> Date of experince</Typography>
-                 </Grid>
-                 <Grid item xs>
-                 <Typography variant="display1"> Category</Typography>
-                 </Grid>
-          </Grid>
-        </ListItemText>
-      </ListItem>
     
-        {/* {this.props.items.map((element, index) => */}
-        {this.props.items.map(({id,name,description, dateOfExp, category}, index) =>
 
-          <ListItem
-            key={index}
-            button
-          // onClick={() => onSelect(id)}
-          >
-            <ListItemText
-              primary={<Grid
-                container>
-                  <Grid item xs>
-                 <Typography variant="subtitle1"> {name}</Typography>
-                 </Grid>
-                 <Grid item xs>
-                 <Typography variant="subtitle1"> {description}</Typography>
-                 </Grid>
-                 <Grid item xs>
-                 <Typography variant="subtitle2"> {dateOfExp}</Typography>
-                 </Grid>
-                 <Grid item xs>
-                 <Typography variant="subtitle2"> {category}</Typography>
-                 </Grid>
-
-              </Grid>}
-
-            />
-            <ListItemSecondaryAction>
-              <IconButton>
-                <Edit />
-              </IconButton>
-              <IconButton    onClick={() => this.props.onDelete(id)
-}>
-                <Delete />
-              </IconButton>
-            </ListItemSecondaryAction>
+        <List component="ul">
+          <ListItem>
+            <ListItemText>
+              <Grid container>
+                <Grid item xs>
+                  <Typography variant="display1">Name</Typography>
+                </Grid>
+                <Grid item xs>
+                  <Typography variant="display1">description</Typography>
+                </Grid>
+                <Grid item xs>
+                  <Typography variant="display1"> Date of experince</Typography>
+                </Grid>
+                <Grid item xs>
+                  <Typography variant="display1"> Category</Typography>
+                </Grid>
+              </Grid>
+            </ListItemText>
           </ListItem>
-        )}
-      </List>
+
+          {this.props.items.map(({ id, name, description, dateOfExp, category }) =>
+
+            // <Item item={{ id, name, description, dateOfExp, category }} onDelete={this.props.onDelete}
+            // key={id} >
+
+              <ListItem
+              key={id}
+              button
+            //  onClick={() => onSelect(id)}
+            >
+              <ListItemText
+                primary={<Grid container>
+                  <Grid item xs>
+                    <Typography variant="subtitle1"> {name}</Typography>
+                  </Grid>
+                  <Grid item xs>
+                    <Typography variant="subtitle1"> {description}</Typography>
+                  </Grid>
+                  <Grid item xs>
+                    <Typography variant="subtitle2"> {dateOfExp}</Typography>
+                  </Grid>
+                  <Grid item xs>
+                    <Typography variant="subtitle2"> {category}</Typography>
+                  </Grid>
+{this.props.editMode
+  ? <Form item={{ name: name, description: description, dateOfExp: dateOfExp, category: category }} />
+  : null}
+                </Grid>}
+
+              />
+              <ListItemSecondaryAction>
+                <IconButton onClick={() => this.props.onSelectEdit(id)}>
+                  <Edit />
+                </IconButton>
+                <IconButton onClick={() => this.props.onDelete(id)}>
+                  <Delete />
+                </IconButton >
+              </ListItemSecondaryAction>
+            </ListItem> 
+              // </Item>
+            
+          )}
+        </List>
+
+
+
+      
     )
   }
 }
