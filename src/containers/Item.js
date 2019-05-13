36ef172896c0d96afefa2b7bcf3a05df85f2515e
EwkaF <obarka@interia.pl>
2019-05-13 09:40:58 +0200

    working on edit form

--- src/containers/Item.js
+++ src/containers/Item.js
@@ -4,9 +4,29 @@ import { Edit, Delete } from '@material-ui/icons';
 import Form from './Form'
 
 export class Item extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editMode: false
+    }
+  }
+
+  handleEdit = () => {
+    this.setState({
+      editMode: ! this.state.editMode
+    })
+  }
+
+  handleSubmit = (details) => {
+    this.props.onSubmit(details)
+    this.handleEdit();
+  }
+
   render() {
+    let date = new Date(this.props.item.dateOfExp);
+    let shortDate = date.getMonth() + 1 + '/' + date.getFullYear();
     return (
-        <ListItem
+      <ListItem
         key={this.props.item.id}
         button
       // onClick={() => onSelect(id)}
@@ -20,19 +40,25 @@ export class Item extends Component {
               <Typography variant="subtitle1"> {this.props.item.description}</Typography>
             </Grid>
             <Grid item xs>
-              <Typography variant="subtitle2"> {this.props.item.dateOfExp}</Typography>
+              <Typography variant="subtitle2"> {shortDate}</Typography>
             </Grid>
             <Grid item xs>
               <Typography variant="subtitle2"> {this.props.item.category}</Typography>
             </Grid>
-{this.props.editMode
-? <Form item={{ name: this.props.item.name, description: this.props.item.description, dateOfExp: this.props.item.dateOfExp, category: this.props.item.category }} />
-: null}
+            {this.state.editMode
+              ? <Form item={{ name: this.props.item.name, description: this.props.item.description, dateOfExp: this.props.item.dateOfExp, category: this.props.item.category }}
+                user={this.props.user}
+                onSelectEdit = {this.handleEdit}
+                button="Save"
+                onSubmit={this.handleSubmit} />
+              : null}
           </Grid>}
 
         />
         <ListItemSecondaryAction>
-          <IconButton onClick={() => this.props.onSelectEdit(this.props.item.id)}>
+          {/* <IconButton onClick={() => this.props.onSelectEdit(this.props.item.id)}> */}
+          <IconButton onClick={() => this.handleEdit()}>
+
             <Edit />
           </IconButton>
           <IconButton onClick={() => this.props.onDelete(this.props.item.id)}>
@@ -40,7 +66,7 @@ export class Item extends Component {
           </IconButton >
         </ListItemSecondaryAction>
 
-        
+
       </ListItem>
     )
   }
